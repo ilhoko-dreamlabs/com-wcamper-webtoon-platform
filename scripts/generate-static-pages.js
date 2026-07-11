a18017888f32a46a3559c181f1b53c47f671356a
DreamLabs Worker <worker@dreamlabs.local>
2026-07-11 19:27:51 +0000

    Update creator mobile hero image

--- scripts/generate-static-pages.js
+++ scripts/generate-static-pages.js
@@ -8,7 +8,7 @@ const SITE_ORIGIN = "https://webtoon.wcamper.com";
 const DEFAULT_OG_IMAGE = "assets/img/thumbnails/bd-crew-episode-01-thumbnail.webp";
 const HOME_MAIN_IMAGE = "assets/img/home/wcamper-home-main-20260709.png";
 const PARTNERSHIP_HERO_IMAGE = "assets/img/partnership-hero.png";
-const ASSET_VERSION = "20260711-creator-hero-mobile";
+const ASSET_VERSION = "20260711-creator-hero-mobile-v2";
 
 function loadCatalog() {
   const catalogPath = path.join(ROOT, "data", "catalog.js");
